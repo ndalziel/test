e89419ece8d3ba7c50fae4e975378824c17a2fee
Nigel Dalziel <ndalziel@seas.upenn.edu>
2019-04-10 08:33:19 -0400

    changed println to print

--- test/src/test/HelloWorld.java
+++ test/src/test/HelloWorld.java
@@ -10,7 +10,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World!");
-		System.out.println("This is the first edit from Angela.");
+		System.out.print("This is the first edit from Angela.");
 
 	}
 
